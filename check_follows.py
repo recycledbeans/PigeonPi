a4159b096eeafafe16f42a1ddb46ed3052271d9b
Matt Watson <matt@portsidedesign.com>
2015-07-14 22:04:29 -0400

    Added files to repository

---
+++ check_follows.py
@@ -0,0 +1,34 @@
+import tweepy
+import os
+import pygame
+
+consumer_key = 'ENTERYOURCONSUMERKEY'
+consumer_secret = 'ENTERYOURCONSUMERSECRET'
+access_token = 'ENTERYOURACCESSTOKEN'
+access_token_secret = 'ENTERYOURACCESSTOKENSECRET'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+me = api.get_user('@recycledbeans');
+
+followers = me.followers_count
+
+file_ = open('current_followers.txt', 'r+')
+
+old_count = file_.read(10)
+
+if int(followers) > int(old_count):
+	print "New Follower!"
+	pygame.mixer.init()
+	pygame.mixer.music.load("dove.mp3")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+		continue
+
+file_.seek(0)
+file_.truncate()
+file_.write(str(followers))
+file_.close()
