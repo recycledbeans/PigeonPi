f1fb7ae4ae0d5b9ba7950f06de1059029b866c25
Matt Watson <matt@portsidedesign.com>
2015-07-17 11:08:48 -0400

    Added comments for clarification

--- check_follows.py
+++ check_follows.py
@@ -1,36 +1,48 @@
-import tweepy
+# Import all of the necessary modules
 import os
-import pygame
-import credentials
 import sys
+import tweepy
+import pygame
+import credentials # <---- Be sure to put your Twitter application's credentials here
 
+# Tweepy OAuth (Authentication)
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
+# Initiate the Tweepy API class
 api = tweepy.API(auth)
 
+# Making sure we have entered a username argument
 try
 	username = sys.argv[0]
 except Exception:
 	print "Don't forget to supply a username!"
 	exit(0)
 
+# Tweepy calls the get_user Twitter API call
 me = api.get_user(username);
 
+# This is the number of our followers (in string form)
 followers = me.followers_count
 
+# Open up a file for caching the username's followers
 file_ = open('followers/' + username + '.txt', 'r+')
 
+# How many followers did we have the last time we checked?
 old_count = file_.read(10)
 
+# If we have more followers:
 if int(followers) > int(old_count):
+	# Show it on the terminal screen
 	print "New Follower!"
+	# Play the including mp3 file
 	pygame.mixer.init()
 	pygame.mixer.music.load("yay.mp3")
 	pygame.mixer.music.play()
 	while pygame.mixer.music.get_busy() == True:
 		continue
 
+# Overwrite the old amount of followers with the new amount
 file_.seek(0)
 file_.truncate()
 file_.write(str(followers))
--- credentials.py
+++ credentials.py
@@ -1,4 +1,15 @@
+####################################################################
+##
+## This is where you put your Twitter application's credentials
+## You can create an application at https://apps.twitter.com/
+##
+####################################################################
+
+# Your Consumer Key
 consumer_key = 'ENTERYOURCONSUMERKEY'
+# Your Consumer Secret
 consumer_secret = 'ENTERYOURCONSUMERSECRET'
+# Your Access Token
 access_token = 'ENTERYOURACCESSTOKEN'
+# Your Access Token Secret
 access_token_secret = 'ENTERYOURACCESSTOKENSECRET'
